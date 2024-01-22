f9d2897ac1cc47b7bccde17bf42274804ec5d104
liux <1531432811@qq.com>
2024-01-22 23:17:49 +0800

    微信登录小程序功能

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,70 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.plaf.TreeUI;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    //GET微信服务接口地址
+    public static final String WX_LOGIN_URL = "https://api.weixin.qq.com/sns/jscode2session";
+
+    /**
+     * 微信登录
+     *
+     * @param userLoginDTO
+     * @return
+     */
+    @Override
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+        String openid = getOpenid(userLoginDTO.getCode());
+        if (openid == null) {
+            //登录失败异常
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+
+        //判断是否为新用户
+        User user = userMapper.getByOpenid(openid);
+
+        if (user == null) {
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+        return user;
+    }
+
+    private String getOpenid(String code){
+        Map<String, String> params = new HashMap<>();
+        params.put("appid", weChatProperties.getAppid());
+        params.put("secret", weChatProperties.getSecret());
+        params.put("js_code", code);
+        params.put("grant_type", "authorization_code");
+        String response = HttpClientUtil.doGet(WX_LOGIN_URL, params);
+        JSONObject jsonObject = JSON.parseObject(response);
+        return jsonObject.getString("openid");
+    }
+}
