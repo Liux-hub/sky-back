8b248d753d3ba63b9c1e1ef4bc0648ba3bc7853d
liux <1531432811@qq.com>
2024-01-23 19:06:35 +0800

    添加redis缓存

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -23,7 +25,8 @@ public class DishController {
 
     @Autowired
     private DishService dishService;
-
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @PostMapping()
     @ApiOperation("新增菜品")
@@ -46,6 +49,8 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("菜品删除：{}",ids);
         dishService.deleteBatch(ids);
+        //清理缓存数据
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -67,6 +72,8 @@ public class DishController {
     public Result update(@RequestBody DishDTO dishDTO){
         log.info("修改菜品{}",dishDTO);
         dishService.updateWithFlavor(dishDTO);
+        //清理缓存数据
+        cleanCache("dish_*");
         return Result.success();
     }
 
@@ -87,6 +94,14 @@ public class DishController {
     @ApiOperation("菜品启用停售")
     public Result<String> startOrStop(@PathVariable Integer status, Long id){
         dishService.startOrStop(status,id);
+        //清理缓存数据
+        cleanCache("dish_*");
         return Result.success();
     }
+
+    private void cleanCache(String pattern){
+        Set keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
+
 }
